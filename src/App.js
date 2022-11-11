37ca7cdcd79e943be3ca20c92b332de3c85e4c36
rashmin66 <hansi20ra@gmail.com>
2022-11-11 23:35:11 +0530

    add details

--- src/App.js
+++ src/App.js
@@ -13,13 +13,26 @@ function App(){
          University of Colombo. As a Computer Science student, I'm looking forward to learning about how Computer science is used in the real
          world. I like to learn new things and improve my skills.<br/>I am able to work under pressure and in a team environment. I
         enjoy working with team members. Moreover, I am a quick learner and would like to broaden my knowledge.</p>
-      <Tweet 
-      details1="Personal information" 
-      details2="Name:Rashmi Nirmani Nimeshika Jayasundara"
-      details3="Birthdate:23/12/1997"
-      details4="Gender:Female"
-      />
-      <Tweet />
+     
+      <h2>Personal Information</h2>
+      <ul><p>Name:Rashmi Nirmani Nimeshika Jayasundara<br/>
+             Birthdate:23/12/1997<br/>
+             Gender   :Female<br/>
+             NIC      :978581366V<br/>
+             Nationality:SriLankan</p><br/></ul>
+      
+      <h2>Education</h2>
+      <h3>1.Bachelor of Computer Science, Statistics and Mathematics in Physical Science(2019 present)</h3>
+      <p><ul> University of Colombo,Colombo<br/>
+              Computer Science, Statistics, Applied Mathematics,Information Technology, Pure Mathematics</ul></p>
+
+      <h3>2.G.C.E Advance Level Examination,2013</h3>
+      <p><ul> Holy Family Balika,Kurunegala<br/>  
+              A:8'S | C:1<br/>
+              Mathematics:A, Science:A, English:C<br/>
+              English:B (G.C.E. O/L in 2018)</ul></p>
+
+  
 
      
     </div>
--- src/tweet.js
+++ src/tweet.js
@@ -6,7 +6,10 @@ function tweet(props){
         <h3>{props.details1}</h3>    
        <ol><p2>{props.details2}</p2><br/>
         <p3>{props.details3}</p3><br/>
-        <p4>{props.details4}</p4></ol>
+        <p4>{props.details4}</p4><br/>
+        <p5>{props.details5}</p5><br/>
+        <p6>{props.details6}</p6><br/>
+        <p7>{props.details7}</p7> </ol>
         
         </div>
     );
